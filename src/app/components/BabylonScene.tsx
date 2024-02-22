b05571a06e0c5ad5699e6273f9354f925f69a87c
acanois <david.richter42@gmail.com>
2024-02-22 13:24:00 -0600

    create starting scene

--- src/app/components/BabylonScene.tsx
+++ src/app/components/BabylonScene.tsx
@@ -9,14 +9,9 @@ import {
   Scene,
 } from '@babylonjs/core'
 import SceneComponent from 'babylonjs-hook'
-import { Attractor } from '../scripts/attractor'
-import { MovingBody } from '../scripts/movingBody'
-
-let attractor: Attractor
-let body: MovingBody
 
 const onSceneReady = (scene: Scene) => {
-  scene.clearColor = new Color4(0, 0, 0, 1.0)
+  scene.clearColor = new Color4(0.2, 0.2, 0.5, 1.0)
 
   const canvas = scene.getEngine().getRenderingCanvas()
   canvas!.width = window.innerWidth
@@ -34,25 +29,10 @@ const onSceneReady = (scene: Scene) => {
 
   const light = new HemisphericLight('light', new Vector3(-1, 1, 1), scene)
   light.intensity = 0.7
-  const light2 = new HemisphericLight('light2', new Vector3(1, -1, -1), scene)
-  light2.intensity = 0.7
-
-  attractor = new Attractor(new Vector3(0, 0, 0), 1.0, scene)
-
-  body = new MovingBody(
-    new Vector3(3.0, 0.0, 3.0), // position
-    new Vector3(-0.01, 0.05, 0.0), // velocity
-    new Vector3(0.0, 0.0, 0.0), // acceleration
-    0.5, // mass
-    0.5, // radius
-    scene
-  )
 }
 
 const onRender = (scene: Scene) => {
-  const force = attractor.attract(body)
-  body.update()
-  body.applyForce(force)
+
 }
 
 function App() {
